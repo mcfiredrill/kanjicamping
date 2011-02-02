8ca6b69a6ce629f99f4585bc7f2edd5dac9d643a
Tony Miller <mcfiredrill@gmail.com>
2011-02-02 12:20:33 -0800

    Initial commit.

---
+++ kanjiapp.rb
@@ -0,0 +1,61 @@
+require 'camping'
+require 'haml'
+require 'nokogiri'
+
+Camping.goes :Kanji
+
+module Kanji
+  module Kanji::Controllers
+
+    class Index
+      def get
+        if(@input.k != nil && @input.k != '')
+          @kanji = @input.k
+          @dic = open_dic
+          @meanings = meanings(@dic, @kanji)
+          @on_readings = on_readings(@dic, @kanji)
+          @kun_readings = kun_readings(@dic, @kanji)
+          @render_result = true
+        elsif
+          @render_result = false
+        end
+        render :index
+      end
+
+    end
+
+    class Style < R '/style\.css'
+      STYLE = File.read('public/style.css')
+
+      def get
+        @headers['Content-Type'] = 'text/css; charset=utf-8'
+        STYLE
+      end
+    end
+
+  end #Controllers
+
+  module Helpers
+      def open_dic
+        f = File.open('kanjidic2.xml')
+        kanjidic = Nokogiri::XML(f)
+        f.close
+        return kanjidic
+      end
+
+      def meanings(d, k)
+        meanings = d.xpath("//literal[text()='"+k+"']/../reading_meaning/rmgroup/meaning[not(@m_lang)]").collect(&:text)
+        return meanings
+      end
+
+      def on_readings(d, k)
+        on_readings = d.xpath("//literal[text()='"+k+"']/../reading_meaning/rmgroup/reading[@r_type='ja_on']").collect(&:text)
+        return on_readings
+      end
+
+      def kun_readings(d, k)
+        kun_readings = d.xpath("//literal[text()='"+k+"']/../reading_meaning/rmgroup/reading[@r_type='ja_kun']").collect(&:text)
+        return kun_readings
+      end
+  end #Helpers
+end
